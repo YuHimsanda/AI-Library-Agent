79a9f8b6a5db01ad58360f87f006d10fd37dfe30
Yuhas <yuhimsanda@gmail.com>
2026-09-24 21:50:40 +0530

    back to simple side bar

--- Streamlit/ui.py
+++ Streamlit/ui.py
@@ -57,7 +57,7 @@ with st.sidebar:
                 conn.close()
                 st.dataframe(df)
             except Exception as e:
-               st.error(f"Error retrieving data: {e}")
+                st.error("Error retrieving data.")
         if st.button("Log Out"):
             st.session_state.admin_logged_in = False
             st.rerun()
